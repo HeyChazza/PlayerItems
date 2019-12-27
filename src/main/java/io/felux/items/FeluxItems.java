6840b5cc45f4dd134474c5deeee8abf51d7c9c35
Chazza <me@chazza.net>
2019-12-27 17:20:18 +0000

    Initial commit

---
+++ src/main/java/io/felux/items/FeluxItems.java
@@ -0,0 +1,17 @@
+package io.felux.items;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FeluxItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
